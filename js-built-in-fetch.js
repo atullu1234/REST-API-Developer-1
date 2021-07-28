d089d4d826aa3732461e522ed8e7a11d262d73e6
Abbu Tulu <atullu1234@gmail.com>
2021-07-28 00:11:45 -0400

    first commit

---
+++ js-built-in-fetch.js
@@ -0,0 +1,97 @@
+/*function getMethod() {
+  fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}*/
+
+//GET
+async function postData(url = "", data = {}) {
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
+
+function getMethod() {
+  postData("https://jsonplaceholder.typicode.com/posts", {
+    userId: 1,
+    id: 1,
+    title:
+      "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+  }).then((data) => {
+    console.log(data);
+  });
+}
+
+//POST
+async function postData(url = "", data = {}) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
+
+function postMethod() {
+  postData("https://jsonplaceholder.typicode.com/posts", {
+    id: 8,
+    petId: 1,
+    quantity: 0,
+    shipDate: "2021-07-23T01:44:32.945Z",
+    status: "placed",
+    complete: false,
+  }).then((data) => {
+    console.log(data);
+  });
+}
+
+//PUT
+async function postData(url = "", data = {}) {
+  const response = await fetch(url, {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
+
+function putMethod() {
+  postData("https://jsonplaceholder.typicode.com/posts/1", {
+    id: 1,
+    petId: 5,
+    quantity: 0,
+    shipDate: "2021-07-23T01:44:32.945Z",
+    status: "placed",
+    complete: false,
+  }).then((data) => {
+    console.log(data);
+  });
+}
+
+//DELETE
+async function postData(url = "", data = {}) {
+  const response = await fetch(url, {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
+
+function deleteMethod() {
+  postData("https://jsonplaceholder.typicode.com/posts/1", {}).then((data) => {
+    console.log(data);
+  });
+}
